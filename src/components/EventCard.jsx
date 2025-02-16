353a13086ccff9f4e0a9baa95b4972607f658ac0
karthik-kiran-29 <karthikkiran29@gmail.com>
2025-02-16 22:25:18 +0530

    Added ExploreEvents.jsx

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
-const EventCard = ()=>{
-
-    return(
-        <h1>Running..</h1>
-    )
-}
-
+const EventCard = ({ title, image, isActive }) => {
+    return (
+      <div className="rounded-lg overflow-hidden">
+        <div 
+          className={"relative h-48 bg-black"}
+        >
+          {
+            <img
+              src={image}
+              alt={title}
+              className="w-full h-full object-cover"
+            />
+          }
+          <div className="absolute bottom-0 left-0 p-4">
+            <h3 className="text-white text-xl font-medium">{title}</h3>
+          </div>
+        </div>
+      </div>
+    );
+  };
 export default EventCard;
